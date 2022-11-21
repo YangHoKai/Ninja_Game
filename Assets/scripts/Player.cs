522d21ad57ade267e0ab813a7c47abd34d2875e8
YangHoKai <117610972+YangHoKai@users.noreply.github.com>
2022-11-21 23:15:13 +0800

    Player程式碼修改
    把左右改成習慣的AD

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -7,19 +7,19 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             transform.Translate(-3, 0, 0); // 往左移動「3」
         }
 
         // 點擊左方向鈕時
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             transform.Translate(3, 0, 0); // 往右移動「3」
         }
