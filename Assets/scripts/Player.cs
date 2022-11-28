54704dc492b8e75b37edbeda1668eeaa7c8c5aa9
YangHoKai <117610972+YangHoKai@users.noreply.github.com>
2022-11-28 09:46:49 +0800

    作業:移動及被擊中時增加聲音
    修改成讓兩種聲音公開放在一個角色裡，並新增受到攻擊時會有聲音

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,9 +6,12 @@ public class Player : MonoBehaviour
 {
     public GameObject gameManager;
     // Start is called before the first frame update
+    AudioSource audiosource;
+    public AudioClip move;
+    public AudioClip gethit;
     void Start()
     {
-
+        audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,27 +20,30 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.Translate(-3, 0, 0); // 往左移動「3」
-            GetComponent<AudioSource>().Play();
+            audiosource.PlayOneShot(move);
         }
 
         // 點擊左方向鈕時
         if (Input.GetKeyDown(KeyCode.D))
         {
             transform.Translate(3, 0, 0); // 往右移動「3」
-            GetComponent<AudioSource>().Play();
+            audiosource.PlayOneShot(move);
         }
         
     }
     public void LButtonDown()
     {
         transform.Translate(-3, 0, 0);
+        audiosource.PlayOneShot(move);
     }
     public void RButtonDown()
     {
         transform.Translate(3, 0, 0);
+        audiosource.PlayOneShot(move);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         gameManager.GetComponent<GameManager>().DecreaseHp();
+        audiosource.PlayOneShot(gethit);
     }
 }
