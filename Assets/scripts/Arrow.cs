54efeb894fabdb946ec987bda3f86ade4fe18e3b
YangHoKai <117610972+YangHoKai@users.noreply.github.com>
2022-11-28 10:22:45 +0800

    作業:新增加分系統
    在Gamemanager新增加分text，並在Arrow的程式抓到Gamemanager，並且碰到addscore的牆壁就會加分

--- Assets/scripts/Arrow.cs
+++ Assets/scripts/Arrow.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Arrow : MonoBehaviour
 {
+    GameObject GameManager;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 3f);
+        GameManager = GameObject.Find("GameManager");
     }
 
     // Update is called once per frame
@@ -17,6 +20,10 @@ public class Arrow : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject );
+        Destroy(gameObject);
+        if(collision.name=="addscore")
+        {
+            GameManager.GetComponent<GameManager>().IncreaseScore(10);
+        }
     }
 }
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -9,10 +9,13 @@ public class GameManager : MonoBehaviour
     float span = 1.0f;
     float delta = 0;
     public GameObject hpGauge;
+
+    int score;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = "分數: 0 分";
     }
 
     // Update is called once per frame
@@ -25,9 +28,17 @@ public class GameManager : MonoBehaviour
             int px = Random.Range(-6, 7);
             Instantiate(arrowprefab, new Vector3(px, 7, 0), Quaternion.identity);
         }
+        
     }
     public void DecreaseHp()
     {
         hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+
     }
+    public void IncreaseScore(int _score)
+    {
+        score += _score;
+        scoreText.text = "分數：" + score + " 分";
+    }
+
 }
